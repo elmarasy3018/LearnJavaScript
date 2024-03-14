5eff6d7e506f031547a427ff1db9e133755c7964
Mohamed EL Marasy <elmarasy3012@gmail.com>
2024-03-14 14:20:55 +0200

    Smallest positive number

--- LearnOnSolo.js
+++ LearnOnSolo.js
@@ -1,3 +1,30 @@
+///// Find the smallest positive number missing /////
+function nSqr(arr) {
+  let i = 1;
+  while (true) {
+    if (arr.indexOf(i) == -1) {
+      return i;
+    }
+    i++;
+  }
+}
+
+function nlogn(arr) {
+  arr = arr.sort();
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] > 1) {
+      return arr[i - 1] + 1;
+    }
+  }
+}
+
+const arr = [-1, 5, 3, 2, 1, 0];
+const nSqrResult = nSqr(arr);
+nSqrResult;
+
+const nlognResult = nlogn(arr);
+nlognResult;
+
 ///// Calculate the clock angle /////
 function calcAngle(hour, minute) {
   if (hour >= 12) {
